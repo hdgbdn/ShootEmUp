f5af13a73f8de8a9b91e8d595ff84b5ff18e8dd1
hdgbdn <hdgbdn92@gmail.com>
2023-04-02 14:19:28 +0900

    add ResourceManager

---
+++ Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,21 @@
+﻿using ShotEmUp;
+//------------------------------------------------------------
+// ShootEmUp
+// Copyright © 2013-2021 Hu Di. All rights reserved.
+// E-mail: hdgbdn92@gmail.com
+//------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShotEmUp
+{
+    public class Manager : MonoBehaviour
+    {
+        protected void Awake()
+        {
+            GameManager.RegisterManager(this);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,49 @@
+﻿//------------------------------------------------------------
+// ShootEmUp
+// Copyright © 2013-2021 Hu Di. All rights reserved.
+// E-mail: hdgbdn92@gmail.com
+//------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ShotEmUp
+{
+    public class ResourceManager : Manager
+    {
+
+        private static string s_uiPathPrefix = "UI/{0}";
+        private static string s_prefabPathPrefix = "Prefabs/{0}";
+
+        public async UniTask<GameObject> LoadUIAsync(string uiName)
+        {
+            string path = string.Format(s_uiPathPrefix, uiName);
+            GameObject go = await LoadInternal<GameObject>(path);
+            go.name = uiName;
+            return go;
+        }
+
+        public async UniTask<GameObject> LoadPrefabAsync(string prefabName)
+        {
+            string path = string.Format(s_prefabPathPrefix, prefabName);
+            GameObject go = await LoadInternal<GameObject>(path);
+            go.name = prefabName;
+            return go;
+        }
+
+        private async UniTask<T> LoadInternal<T>(string path) where T : UnityEngine.Object
+        {
+            var asyncOperation = Resources.LoadAsync(path, typeof(T));
+            await asyncOperation;
+            T resource = asyncOperation.asset as T;
+            if (resource == null)
+            {
+                Debug.LogWarning(string.Format("Asset: {0} load failed!", path));
+            }
+            return resource;
+        }    
+    }
+}
+
